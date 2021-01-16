a5d4af75fce03d6c38594e8752b173482c5936b2
Gareth Field <field.gareth@gmail.com>
2021-01-16 05:12:47 -0500

    the player moves, about to refactor!

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { timer } from 'd3-timer';
 import Container from './components/Container';
 import GameBoard, { positions } from './components/GameBoard';
 import Global from './components/Global';
@@ -16,13 +17,18 @@ function arrayEquals(a, b) {
 }
 
 const App = () => {
-	const [playerPosition, setPlayerPosition] = useState([9, 3]);
+	const [playerPosition, setPlayerPosition] = useState([9.0, 3.0]);
 	const [monsterPositions, setMonsterPositions] = useState([
 		[7, 7],
 		[7, 8],
 		[7, 9],
 		[7, 10],
 	]);
+	const [topSpeed, setTopSpeed] = useState(400);
+	// put your setPositions in
+	const tickAnimation = () => {
+		// setPlayerPosition(prev => [prev[0] - 0.1, prev[1] + 0.1]);
+	};
 	const handleKeyDown = e => {
 		// const up = 38;
 		// const down = 40;
@@ -59,10 +65,14 @@ const App = () => {
 		}
 	};
 	useEffect(() => {
+		const t = timer(tickAnimation);
 		document.addEventListener('keydown', handleKeyDown);
 		return function cleanup() {
 			document.removeEventListener('keydown', handleKeyDown);
 		};
+		// return () => t.stop()
+		// TODO HOW DO I CLEAN BOTH OF THESE UP
+		// keyup
 	}, []);
 	return (
 		<>
